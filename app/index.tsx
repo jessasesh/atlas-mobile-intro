cfba277f3340327683a581f15fbae483acbd8b41
jessasesh <jessica.dison@yahoo.com>
2025-02-19 23:46:08 -0600

    Added button and styling to link home page to add-activities page

---
+++ app/index.tsx
@@ -0,0 +1,42 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function HomeScreen() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push('/add-activity')}>
+        <Text style={styles.buttonText}>Add Activity</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    width: '100%',
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
